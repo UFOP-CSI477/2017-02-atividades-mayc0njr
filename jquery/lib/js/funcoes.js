e1dcf437e1503283af508b504c8e38c0896b2897
Maycon Miranda <mayconjr_@live.com>
2017-12-12 21:56:21 -0200

    jquery

--- jquery/lib/js/funcoes.js
+++ jquery/lib/js/funcoes.js
@@ -3,5 +3,25 @@
 
 $(document).ready(() => {
     //todo conteudo do jQuery
-    console.log("Documento pronto para uso");
+    console.log("Documento pronto para uso")
+    
+    //"p" = seletor de classe p || ocultando P ao click.
+    $("p").click(function(){
+        console.log("this: " + this);
+        $(this).hide();
+    })
+    
+    $("button[name='exibir']").click(()=>{
+        $("#links").show()
+    });
+    $("button[name='ocultar']").click(()=>{
+        $("#links").hide()
+    });
+
+    $("button[name='mudar']").click(()=>{
+        $("#titulo").addClass("jumbotron")
+        $("button").addClass("btn btn-primary");
+        $("#links").addClass("alert alert-info");
+        $("p").removeClass("h2");
+    });
 })
