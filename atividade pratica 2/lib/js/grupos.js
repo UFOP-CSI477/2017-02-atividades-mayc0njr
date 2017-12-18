239aa04c25c60238135879f3bd19f7af1bdbf04f
Maycon Miranda <mayconjr_@live.com>
2017-12-17 23:00:05 -0200

    atividade pratica 2

--- atividade pratica 2/lib/js/grupos.js
+++ atividade pratica 2/lib/js/grupos.js
@@ -10,10 +10,44 @@ Array.prototype.pushIfNotExist = function(element) {
 
 $(document).ready(function(){
     var items = []
-    $(".clickable-div").click((event) =>{
+    var sel1, sel2, sel3, sel4; //selecteds
+    var cor1 = "sabonete"; var cor2 = "algodao"; var cor3 = "maquina"; var cor4 = "chave" //corrects
+    var pontos = 0;
+    $(".clickable-g1").click((event) =>{
         var itm = event.target.name == undefined ? event.target.id : event.target.name;
         items.pushIfNotExist(itm);
-        alert(itm);
+        if(sel1 != undefined){
+            sel1.classList.remove("selected");
+            sel1.classList.add("option-border");
+        }
+        sel1 = document.getElementById(itm);
+        sel1.classList.add("selected");
+        console.log("event: " + event);
+        console.log(items);
+    })
+
+    $(".clickable-g2").click((event) =>{
+        var itm = event.target.name == undefined ? event.target.id : event.target.name;
+        items.pushIfNotExist(itm);
+        if(sel2 != undefined){
+            sel2.classList.remove("selected");
+            sel2.classList.add("option-border");
+        }
+        sel2 = document.getElementById(itm);
+        sel2.classList.add("selected");
+        console.log("event: " + event);
+        console.log(items);
+    })
+
+    $(".clickable-g3").click((event) =>{
+        var itm = event.target.name == undefined ? event.target.id : event.target.name;
+        items.pushIfNotExist(itm);
+        if(sel3 != undefined){
+            sel3.classList.remove("selected");
+            sel3.classList.add("option-border");
+        }
+        sel3 = document.getElementById(itm);
+        sel3.classList.add("selected");
         console.log("event: " + event);
         console.log(items);
     })
